973b0e291ed8b1829a0d85fbee9e80da503c9236
mohit2417 <147239048+mohit2417@users.noreply.github.com>
2024-06-15 00:27:17 +0530

    Update gemini.js

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -7,7 +7,7 @@ import {
   const MODEL_NAME = "gemini-1.0-pro";
   
   // Paste Your API KEY Below
-  const API_KEY = "AIzaSyB6tQnVlgqO9fvHtwCGKoIdNfkNHSZJlME";
+  const API_KEY = "your api";
   
   async function runChat(prompt) {
     const genAI = new GoogleGenerativeAI(API_KEY);
